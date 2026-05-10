a44fe768b034095f381fd876b46a9d00b9974e1f
luisfonsivevo <jerbear3.14159@gmail.com>
2026-05-10 12:12:24 -0600

    update borger

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,10 +1,11 @@
 import * as Borger from "@borger/ts";
 
-const engine = await Borger.init();
-
-//purely client sided rendering pipeline. it should
-//be able to able to render the game in any state,
-//regardless of what the simulation is doing. remember
-//that rollbacks/mispredicts/reconnects can wipe out
-//data that was already rendered in a previous frame
-await Borger.present(engine, function presentationLoop(input: Borger.Input, output: Borger.Output) {});
+await Borger.play(function game() {
+	//purely client sided rendering pipeline. it should
+	//be able to able to render the game in any state,
+	//regardless of what the simulation is doing. remember
+	//that rollbacks/mispredicts/reconnects can wipe out
+	//data that was already rendered in a previous frame
+	//eslint-disable-next-line @typescript-eslint/no-unused-vars
+	return function presentationLoop(dt, input, output) {};
+});
