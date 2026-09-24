ea4a6d8a09a484e366271262cf3ab0276040607f
luisfonsivevo <jerbear3.14159@gmail.com>
2026-09-23 18:33:36 -0600

    type system extensibility via plugins

--- eslint.config.ts
+++ eslint.config.ts
@@ -15,7 +15,7 @@ export default [
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
 			parserOptions: {
-				project: ["tsconfig.presentation.json", "borger/tsconfig.json"],
+				project: ["tsconfig.presentation.json", "tsconfig.node.json"],
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
